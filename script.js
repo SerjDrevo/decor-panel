8e37c6a23285e5602b271aa4b60e0df9bdaa6e22
Sergey Drevoline <sergey.drevoline@gmail.com>
2021-12-24 13:04:20 +0200

    change

---
+++ script.js
@@ -0,0 +1,9 @@
+var numtop = 4;
+$('.decor-panel .decor-panel__menu > li:lt(' + numtop + ')').css( "display", "block" );
+$('.pagination-btn').click(function () {
+    $('.decor-panel .decor-panel__menu > li:lt(' + (numtop += 8) + ')').fadeIn();
+    if (numtop > $('.decor-panel .decor-panel__menu > li').length) {
+        $('.decor-panel .decor-panel__menu > li:lt(' + (numtop += 8) + ')').fadeIn();
+        $(this).fadeOut();
+    }
+});
